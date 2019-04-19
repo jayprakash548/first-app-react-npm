22fedd0086a7cb9c2a07448da6d7c97ea5819c2d
Jay Prakash Kumar <jay.kasyap@live.com>
2019-04-20 00:43:18 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// let names = <h1>Hello Jay Prakash Kumar! You are a Good Developer</h1>
+class App extends Component{
+    render(){
+        return <h1>Hi Component!</h1>
+    }
 }
-
 export default App;
+
+
