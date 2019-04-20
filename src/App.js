ba4e6fb78e41ef68ab031309b53091f74c0bd844
Jay Prakash Kumar <jay.kasyap@live.com>
2019-04-20 17:45:08 +0530

    return multiple elements using fragements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 // let names = <h1>Hello Jay Prakash Kumar! You are a Good Developer</h1>
 class App extends Component{
     render(){
-        return <h1>Hi Component!</h1>
-    }
-}
+        return (
+            <Fragment>
+                <h1>Introduction</h1>
+                <p>My Name is Jay Prakash Kumar</p>
+            </Fragment>
+            // <> // Not support to every Browser
+            //     <h1>Introduction</h1>
+            //     <p>My Name is Jay Prakash Kumar</p>
+            // </>
+        );
+    }; 
+};
 export default App;
 
 
