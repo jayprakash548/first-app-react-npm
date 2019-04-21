7e21bf7c0d002639f193fb0ca0150118cf5b94a1
Jay Prakash Kumar <jay.kasyap@live.com>
2019-04-21 23:08:05 +0530

    Children in JSX using functional and class components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
 
- const App = (props) =>{
-     return(
-        <div>
-            <h1>Hello {props.name}</h1>
-        </div>
-     )
- }
-App.propType = {
-    name: PropTypes.string
+// const App = props =>{
+//     return <h1>Hello Jay Prakash Kumar {props.children}</h1>
+// }
+
+// export default App;
+
+import React, {Component} from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello Mr. Jay!. {this.props.children}</h1>
+            </div>
+        )
+    }
 }
 
-App.propTypes = {
-    name: PropTypes.string.isRequired
-}
-App.defaultProps = {
-    name: "Sony"
-}
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'   ;
 
-ReactDOM.render(<App name="Jay"/>, document.getElementById('root'));
+ReactDOM.render(<App> Your children will be very Intelligent </App>, document.getElementById('root'));
