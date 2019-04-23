37812c3074d9ece39737fe70ac74be349b7b4f2e
Jay Prakash Kumar <jay.kasyap@live.com>
2019-04-23 07:53:16 +0530

    State in React

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-// import React from 'react';
-
-// const App = props =>{
-//     return <h1>Hello Jay Prakash Kumar {props.children}</h1>
-// }
-
-// export default App;
-
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "Rahul",
+            city: this.props.city 
+        }
+    }
+    /* state = {
+        name: "Rahul",
+        city: this.props.city
+    } */
     render(){
         return(
-            <div>
-                <h1>Hello Mr. Jay!. {this.props.children}</h1>
-            </div>
+            <h1>Hello {this.state.name}. Your city is {this.state.city}</h1>
         )
     }
 }
 
-export default App;
+export default App
